06d924b61b6a277e6745a772008c63a673a24cc6
Enzo Santos <enzo.santos29@hotmail.com>
2023-11-12 19:54:13 -0300

    Adiciona parâmetro `encoding` em CSV

--- pyplus/io/i/_csv.py
+++ pyplus/io/i/_csv.py
@@ -11,8 +11,9 @@ def _read_lines(
     *,
     delimiter: str = ";",
     skiprows: int = 0,
+    encoding: str = "utf-8",
 ) -> typing.Iterator[list[str]]:
-    with open(fpath, encoding="utf-8") as f:
+    with open(fpath, encoding=encoding) as f:
         reader: typing.Iterator[list[str]]
         reader = csv.reader(f, delimiter=delimiter)
         if skiprows:
@@ -26,9 +27,10 @@ def _read_models(
     *,
     delimiter: str = ";",
     header: int = 0,
+    encoding: str = "utf-8",
     converter: typing.Callable[[dict[str, str]], M],
 ) -> typing.Iterator[M]:
-    with open(fpath, encoding="utf-8") as f:
+    with open(fpath, encoding=encoding) as f:
         file: typing.Iterator[str] = f
         if header:
             file = itertools.islice(file, header, None)
